2b5b06505432af0cf5eb12d7bad49b8a216ece6e
Flávio Cardoso <flaviopc2@gmail.com>
2026-03-29 08:59:49 -0300

    primeito commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,51 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+   imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cardapio-digital');
+
+  titulo = 'RV Distribuidora 24hs';
+
+  produtos = [
+    {
+      nome: 'Batata Simples',
+      descricao: 'Pão, carne, queijo e molho especial',
+      preco: 15.00,
+      imagem: 'assets/produtos/batata-simples.jpg'
+    },
+    {
+      nome: 'Batata com Calabresa',
+      descricao: 'Pão, carne, queijo, alface e tomate',
+      preco: 18.00,
+      imagem: 'assets/produtos/batata-com-calabresa.jpg'
+    },
+    {
+      nome: 'Batata com Frango',
+      descricao: 'Lata 350ml',
+      preco: 6.00,
+      imagem: 'assets/produtos/batata-frango.jpg'
+    },
+    {
+      nome: 'Isca de Frango',
+      descricao: 'Porção média crocante',
+      preco: 12.00,
+      imagem: 'assets/produtos/frango.jpg'
+    }
+  ];
+
+  pedir(produto: any) {
+    const numero = '5521974863278';
+
+    const mensagem = `Olá, quero pedir:\n${produto.nome} - R$ ${produto.preco.toFixed(2)}`;
+
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(url, '_blank');
+  }
 }
